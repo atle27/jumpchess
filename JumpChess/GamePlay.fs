32bde9c97e01417b359b723cf9d48a1599e5e0a4
Atle Wilhelmsen <atle.wilhelmsen@gmail.com>
2017-03-12 14:54:36 -0400

    Added GamePlay module with function to calculate all legal moves from a given game position.

---
+++ JumpChess/GamePlay.fs
@@ -0,0 +1,34 @@
+﻿module JumpChess.GamePlay
+
+open JumpChess.MarbleLane
+open JumpChess.MarbleJump
+open JumpChess.GameBoard
+
+type GamePlayer = {
+    color:MarbleColor
+    order:int // 0..5
+    marbles:(HoleCoord Set) }
+
+type Game = {
+    board:GameBoard
+    players:GamePlayer list
+    isSuperJump:bool }
+
+type Move = HoleCoord list
+
+let rec private movesSpan (moves:Move) (game:Game) =
+    let hole = moves.Head 
+    let lane = game.board.[hole.axis].[hole.row]
+    seq {
+        for rotation in 0..2 do
+            let rotatedHole = rotatedLaneCoord lane.Length rotation hole
+            let rotatedLane = game.board.[rotatedHole.axis].[rotatedHole.row]
+            let jumpIndices = jumpIndices rotatedLane rotatedHole.index game.isSuperJump
+            if Seq.isEmpty jumpIndices 
+            then yield moves
+            else for index in jumpIndices do
+                    let nextHole = { rotatedHole with index = index }
+                    let newMove = nextHole :: moves
+                    yield newMove }
+            
+let allMoves (hole:HoleCoord) = movesSpan [hole]

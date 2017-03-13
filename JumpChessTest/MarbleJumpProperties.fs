61b4039acfa6d96ad8724f122a108d5236938368
Atle Wilhelmsen <atle.wilhelmsen@gmail.com>
2017-03-12 22:06:39 -0400

    Fixed isValidJump function such that ``All jumps are valid`` propery test runs successfull for regular jumps. For super jump the test function isValidJump still needs to be fixed.

--- JumpChessTest/MarbleJumpProperties.fs
+++ JumpChessTest/MarbleJumpProperties.fs
@@ -18,23 +18,33 @@ let printLane lane =
     for hole in lane do printHole hole
     Debug.WriteLine ""
     
-let rec private emptyHolesInRange (lane:MarbleLane) (fromIndex:int) (toIndex:int) =
+let private subLane (lane:MarbleLane) (fromIndex:int) (toIndex:int) =
     Array.sub lane (Math.Min(fromIndex,toIndex)) (Math.Abs(fromIndex-toIndex)+1)
+
+let rec private emptyHolesInRange lane fromIndex toIndex =
+    subLane lane fromIndex toIndex 
     |> Array.fold (fun count hole -> if hole = Empty then count + 1 else count) 0
 
 let isValidJump (isSuperJump:bool) (marbleLane:MarbleLane) (fromIndex:int) (toIndex:int) =
         if (Math.Abs(fromIndex - toIndex) = 1)
         then marbleLane.[toIndex] = Empty
         else
-            if not isSuperJump && Math.Abs(fromIndex - toIndex) > 2
-            then false
-            else
-                let direction = if toIndex > fromIndex then 1 else -1
+            let direction = if toIndex > fromIndex then 1 else -1
+            if isSuperJump
+            then // only validates a single super jump
                 let jumpHoles = Math.Abs(fromIndex-toIndex)
                 let emptyHoles = emptyHolesInRange marbleLane fromIndex toIndex
                 let allHolesEmptyButOne = emptyHoles = jumpHoles-1
                 let middleHoleNotEmpty = marbleLane.[fromIndex + ((toIndex - fromIndex) / 2)] <> Empty 
                 allHolesEmptyButOne && middleHoleNotEmpty
+            else
+                let jumpArray = subLane marbleLane fromIndex toIndex
+                if direction < 0 then Array.rev jumpArray else jumpArray
+                |> Array.mapi (fun index hole -> 
+                    if index = 0 || isOdd index
+                    then hole <> Empty
+                    else hole = Empty)
+                |> Array.forall (fun x -> x)
 
 let laneLengthRange = Gen.elements [1..13] |> Arb.fromGen
 

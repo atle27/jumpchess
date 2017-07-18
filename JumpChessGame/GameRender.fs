026b4d2821519d249cc05f20662a02cb30bee02b
Atle Wilhelmsen <atle.wilhelmsen@gmail.com>
2017-07-18 08:38:55 -0400

    Move game render functionality to separate module.

---
+++ JumpChessGame/GameRender.fs
@@ -0,0 +1,43 @@
+﻿module JumpChess.GameRender
+
+open System
+open JumpChess.Common
+open JumpChess.MarbleLane
+open JumpChess.GameBoard
+
+let private renderMarbleHole (marbleHole:MarbleHole) =
+    let marbleConsoleColor = 
+        match marbleHole with
+        | Marble(Black) -> Some(ConsoleColor.DarkMagenta)
+        | Marble(White) -> Some(ConsoleColor.White)
+        | Marble(Red) -> Some(ConsoleColor.Red)
+        | Marble(Blue) -> Some(ConsoleColor.Blue)
+        | Marble(Green) -> Some(ConsoleColor.Green)
+        | Marble(Yellow) -> Some(ConsoleColor.Yellow)
+        | Empty -> None
+    Console.Write " "
+    match marbleConsoleColor with
+    | Some(color) -> 
+        Console.BackgroundColor <- color
+        Console.Write "  "
+        Console.BackgroundColor <- ConsoleColor.Black
+    | None ->
+        Console.Write "()"
+    Console.Write " "
+
+let private renderGameBoard (gameBoard:GameBoard) =
+    Console.WriteLine " "
+    for row in { 0 .. 16 } do
+        let rowLength = gameBoard.[0].[row].Length
+        let paddingCount = (13 - rowLength) / 2
+        if isOdd rowLength 
+        then Console.Write " "
+        else Console.Write "   "
+        for _ in { 1 .. paddingCount } do Console.Write "    "
+        for index in { 0 .. rowLength-1 } do
+            renderMarbleHole gameBoard.[0].[row].[index]
+        Console.Write "\n \n"
+
+type Render() =
+    static member drawConsole : (GameBoard -> unit) = fun board -> renderGameBoard board
+        
--- JumpChessGame/Program.fs
+++ JumpChessGame/Program.fs
@@ -4,45 +4,13 @@ open JumpChess.MarbleLane
 open JumpChess.GameBoard
 open JumpChess.GamePlay
 open JumpChess.GameStrategy
+open JumpChess.GameRender
 
-let renderMarbleHole (marbleHole:MarbleHole) =
-    let marbleConsoleColor = 
-        match marbleHole with
-        | Marble(Black) -> Some(ConsoleColor.DarkMagenta)
-        | Marble(White) -> Some(ConsoleColor.White)
-        | Marble(Red) -> Some(ConsoleColor.Red)
-        | Marble(Blue) -> Some(ConsoleColor.Blue)
-        | Marble(Green) -> Some(ConsoleColor.Green)
-        | Marble(Yellow) -> Some(ConsoleColor.Yellow)
-        | Empty -> None
-    Console.Write " "
-    match marbleConsoleColor with
-    | Some(color) -> 
-        Console.BackgroundColor <- color
-        Console.Write "  "
-        Console.BackgroundColor <- ConsoleColor.Black
-    | None ->
-        Console.Write "()"
-    Console.Write " "
-
-let renderGameBoard (gameBoard:GameBoard) =
-    Console.WriteLine " "
-    for row in { 0 .. 16 } do
-        let rowLength = gameBoard.[0].[row].Length
-        let paddingCount = (13 - rowLength) / 2
-        if isOdd rowLength 
-        then Console.Write " "
-        else Console.Write "   "
-        for _ in { 1 .. paddingCount } do Console.Write "    "
-        for index in { 0 .. rowLength-1 } do
-            renderMarbleHole gameBoard.[0].[row].[index]
-        Console.Write "\n \n"
-        
 [<EntryPoint>]
 let main argv = 
     Console.SetWindowSize (55, 36)
 
-    let marbleToMoveCoord = { index = 7; row = -1; rot = 0 } 
+    let greenMarbleCoord = { index = 7; row = -1; rot = 0 } 
 
     let gameBoard = 
         Board.create 
@@ -51,23 +19,25 @@ let main argv =
         |> Board.add Red { index = 5; row = 0; rot = 0 }
         |> Board.add Red { index = 6; row = -1; rot = 2 } 
         |> Board.add Red { index = 5; row = 4; rot = 0 } 
-        |> Board.add Green marbleToMoveCoord 
+        |> Board.add Green greenMarbleCoord 
 
-    renderGameBoard gameBoard
-
-    let game = { board = gameBoard; players = []; isSuperJump = true }
+    Render.drawConsole <| gameBoard
 
     Console.WriteLine "Press a key to see all legal moves for green marble..."
 
     Console.ReadKey() |> ignore
+   
+    let game = { board = gameBoard; players = []; isSuperJump = true }
 
-    let rec boardWithMoves (board:GameBoard) (moves:Move list) =
+    let allMovesGreenMarble = Strategy.allMoves game greenMarbleCoord |> Seq.toList
+
+    let rec addMarblesForMovesToBoard (board:GameBoard) (moves:Move list) =
         match moves with
         | move::rest ->
-            boardWithMoves (board |> Board.add White move.Head) rest
+            addMarblesForMovesToBoard (board |> Board.add White move.Head) rest
         | [] -> board
 
-    renderGameBoard (boardWithMoves gameBoard (Seq.toList(Strategy.allMoves game marbleToMoveCoord)))
+    Render.drawConsole <| (addMarblesForMovesToBoard gameBoard allMovesGreenMarble)
 
     Console.ReadKey() |> ignore
 
